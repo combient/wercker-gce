a0fc86aa293a70b5642b1f648bfeeff535ca943c
oluies <orjan.lundberg@gmail.com>
2016-01-21 10:18:09 +0100

    gcloud ReplicationController

---
+++ createcontrollerscript.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+cat > controller.json <<EOF
+{
+  "kind": "ReplicationController",
+  "apiVersion": "v1beta3",
+  "metadata": {
+    "name": "helloworld",
+    "labels": {
+      "name": "helloworld"
+    }
+  },
+  "spec": {
+    "replicas": 3,
+    "selector": {
+      "name": "helloworld"
+    },
+    "template": {
+      "metadata": {
+        "labels": {
+          "name": "helloworld",
+          "deployment": "${WERCKER_GIT_COMMIT}"
+        }
+      },
+      "spec": {
+        "containers": [
+          {
+            "imagePullPolicy": "Always",
+            "image": "${GCLOUD_REPOSITORY}:${WERCKER_GIT_COMMIT}",
+            "name": "helloworld",
+            "ports": [
+              {
+                "name": "http-server",
+                "containerPort": 8080,
+                "protocol": "TCP"
+              }
+            ]
+          }
+        ]
+      }
+    }
+  }
+}
+EOF
