29dbd576919159e5ece5779cf3e72976964d282d
oluies <orjan.lundberg@gmail.com>
2016-01-23 23:45:24 +0100

    gcloud json

--- createcontrollerscript.sh
+++ createcontrollerscript.sh
@@ -2,43 +2,42 @@
 
 cat > controller.json <<EOF
 {
-  "kind": "ReplicationController",
-  "apiVersion": "v1",
-  "metadata": {
-    "name": "helloworld",
-    "labels": {
-      "name": "helloworld"
-    }
-  },
-  "spec": {
-    "replicas": 3,
-    "selector": {
-      "name": "helloworld"
-    },
-    "template": {
-      "metadata": {
-        "labels": {
-          "name": "helloworld",
-          "deployment": "${WERCKER_GIT_COMMIT}"
-        }
-      },
-      "spec": {
-        "containers": [
-          {
-            "imagePullPolicy": "Always",
-            "image": "${GCLOUD_REPOSITORY}:${WERCKER_GIT_COMMIT}",
-            "name": "helloworld",
-            "ports": [
-              {
-                "name": "http-server",
-                "containerPort": 8080,
-                "protocol": "TCP"
-              }
-            ]
-          }
-        ]
+   "kind":"ReplicationController",
+   "apiVersion":"v1",
+   "metadata":{
+      "name":"helloworld",
+      "labels":{
+         "app":"helloworld"
       }
-    }
-  }
+   },
+   "spec":{
+      "replicas":3,
+      "selector":{
+         "app":"helloworld"
+      },
+      "template":{
+         "metadata":{
+            "labels":{
+               "app":"helloworld"
+               "deployment": "${WERCKER_GIT_COMMIT}"
+            }
+         },
+         "spec":{
+            "containers":[
+               {
+                  "name":"helloworld",
+                  "image": "${GCLOUD_REPOSITORY}:${WERCKER_GIT_COMMIT}",
+                  "ports":[
+                     {
+                        "name": "http-server",
+                        "containerPort": 8080,
+                        "protocol": "TCP"
+                     }
+                  ]
+               }
+            ]
+         }
+      }
+   }
 }
 EOF
