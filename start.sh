774a167577828f4a230898e2001d7c5a69ed0a00
oluies <orjan.lundberg@gmail.com>
2016-02-02 17:49:14 +0100

    cd to target in start script

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+cd /target
 java -jar /target/dropwizard-example-1.0.0-SNAPSHOT.jar db migrate /target/example.yml &
 sleep 5
 java -jar /target/dropwizard-example-1.0.0-SNAPSHOT.jar server /target/example.yml
